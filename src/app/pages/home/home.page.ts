4d989248615c40366bb362efc86516169d42fb6d
Àlex <alex@venomapps.com>
2022-01-19 23:34:03 +0100

    Initial commit for Ionic app.

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Components {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  components: Components[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+    {
+      icon: 'beaker-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar'
+    },
+    {
+      icon: 'radio-button-off-outline',
+      name: 'Button',
+      redirectTo: '/button'
+    },
+    {
+      icon: 'card-outline',
+      name: 'Card',
+      redirectTo: '/card'
+    },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checkbox',
+      redirectTo: '/checkbox'
+    },
+    {
+      icon: 'calendar-outline',
+      name: 'Datetime',
+      redirectTo: '/datetime'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
